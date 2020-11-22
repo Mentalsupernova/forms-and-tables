bc24050ad7cc031059364481352ea5b1299f1d23
mentalsupernova <mentalsupernovafox@gmail.com>
2020-11-22 13:40:46 +0300

    init

---
+++ src/components/Demo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="width: 600px">
+    <vue-table-dynamic 
+      :params="params"
+      @cell-change="onCellChange"
+      ref="table"
+    >
+    </vue-table-dynamic>
+  </div>
+</template>
+
+<script>
+import VueTableDynamic from 'vue-table-dynamic'
+export default {
+  name: 'Demo',
+  data() {
+    return {
+      params: {
+        data: [
+          ['Дата', 'таск', 'статус',],
+          [10.02, 'Купить хлеба', '2'],
+          [10.02, 'Построить реактор', '1', ],
+          [10.02, 'Захватить мир', '404', ]
+        ],
+        header: 'row',
+        edit: {
+          row: [1],
+          column: [1],
+          cell: [[-1, -1]]
+        }
+      }
+    }
+  },
+  methods: {
+    onCellChange (rowIndex, columnIndex, data) {
+      console.log('onCellChange: ', rowIndex, columnIndex, data)
+      console.log('table data: ', this.$refs.table.getData())
+    }
+  },
+  components: { VueTableDynamic }
+}
+</script>
